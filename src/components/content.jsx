6599878ed34d25efb48ef229b903edcd77f4444d
Aurora Powell <33322696+AuroraPowell@users.noreply.github.com>
2024-08-19 15:08:18 -0400

    Revert "Delete src directory"
    This reverts commit 4f3c08e14820edb15915dad2e00852c7c3bca0eb.

---
+++ src/components/content.jsx
@@ -0,0 +1,14 @@
+import LoginButton from './LoginButton'
+import LogoutButton from './LogoutButton'
+import EventList from './eventComponents/eventList'
+
+function Content() {
+
+    return (
+      <main>
+        <EventList />
+      </main>
+    )
+  }
+  
+  export default Content
---
+++ src/components/head.jsx
@@ -0,0 +1,12 @@
+function Header(){
+
+    return(
+        <>
+            <h1 className='float-start p-3 text-3xl text-white font-bold rounded-full'>
+                Count Me In
+            </h1>
+        </>
+    )
+}
+
+export default Header
