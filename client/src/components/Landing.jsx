6ca52923be5f49f7f4329c4700be9fe2dd9c917a
Aurora Powell <33322696+AuroraPowell@users.noreply.github.com>
2025-01-01 13:32:37 -0500

    Updated Landing (art portfolio)
    Creating landing page as art portfolio, combining my interests.
    
    I'll utilize a grid card system that will allow users to click thumbnails to bring up a larger modal display.
    
    Additionally, i need to render the grid systems differently across various screen sizes. In the projects page, I will create both desktop and mobile version of the project title cards.

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -12,12 +12,26 @@ const Landing = () => {
 
   const Content = () => {
     return (
-      <div id="container" className="block flex-1 w-4/5 h-full mx-auto">
+      <div id="container" className="block flex-1 w-full px-10 h-full mx-auto">
         {/* <div id="landing-top" className=""><p>Thanks for checking out my portfolio.<br></br>Please have a look around. You never know what halls I'll be excavating out next.</p></div> */}
-        <div id="shape" className="border-4 inset-32">
+        <div id="shape" className="border-4 m-0.5">
           
-          <div id="upper" className="bg-red-900 align-bottom text-white text-3xl font-bold min-h-72 m-9">
-            
+          <div id="upper" className="bg-slate-100 bg-opacity-50 align-bottom 
+          text-white text-3xl font-bold 
+          min-h-72 m-9 flex flex-row">
+            <div id="sidebar" className="1/5 bg-cyan-900">
+              <ul>
+                <li>Series One</li>
+                <li>Series Two</li>
+              </ul>
+            </div>
+            <div id="artContainer" className="4/5 bg-slate-600 w-full
+            grid grid-cols-2">
+              <p className="bg-red-400 text-black">include a change to styling</p>
+              <p className="bg-cyan-400 ">hello</p>
+              <p className="bg-red-400 ">hello</p>
+              <p className="bg-cyan-400 ">hello</p>
+            </div>
           </div>
           <div id="lower"></div>
 
