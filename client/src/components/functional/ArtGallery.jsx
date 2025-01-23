edf4582db9622d564c6935ff2e5a9fd13c8d586a
Aurora Powell <33322696+AuroraPowell@users.noreply.github.com>
2025-01-22 22:19:19 -0500

    Working still
    Floundering

--- client/src/components/functional/ArtGallery.jsx
+++ client/src/components/functional/ArtGallery.jsx
@@ -1,72 +1,70 @@
-import React from 'react'
-import { useState } from 'react';
+import React from "react";
+import { useState, useEffect } from "react";
 
-import data from '../art-pieces/art.json'
-import Modal from '../projects/countme.in/functions/Modal';
+import data from "../art-pieces/art.json";
+import Modal from "../projects/countme.in/functions/Modal";
 
 const ArtGallery = () => {
+  const [open, setOpen] = useState(false);
 
+  const [artFocus, setArtFocus] = useState();
+  const [seriesFocus, setSeriesFocus] = useState();
 
-    const [open, setOpen] = useState(false)
-    
-    const [artFocus, setArtFocus] = useState();
-    const [seriesFocus, setSeriesFocus] = useState();
-    
-    
+  // creates responsively created list from mapping JSON file
+  const CreateGallery = () => {
+    let seriesTitles = [];
+    let list = [];
 
+    data.map((art) => {
+      if (!seriesTitles.includes(art.series)) {
+        seriesTitles.push(art.series);
+      }
+    });
 
-    // creates responsively created list from mapping JSON file
-    const CreateGallery = () => {
-      console.log(seriesFocus)
+    const createList = () => {
+      return <div></div>;
+    };
 
-      let series = []
-
-      
-      
-      let listOfArt = data.map( ( art ) => {
-        if(!series.includes(art.series)){
-          series.push(art.series)
-        }
-
-  
-      });
-      
-    
-
-      return (
-        <div className="text-slate-100 text-8xl italic font-bold text-end my-auto px-36">
-          <div className='flex justify-evenly '>
-
-            {series.map( ( series ) => {
-              return(
-                <button key= { series } 
-                  className='text-5xl  hover:text-slate-900'
-                  onClick={ () => ( setSeriesFocus(series) ) }>
-                   { series }
-                </button>
-              )
-            })}
-          </div>
-        </div>
-      )
+    function handleClick(selection) {
+      setSeriesFocus(selection);
     }
+
+    return (
+      <div className="text-slate-100 text-8xl italic font-bold text-end my-auto px-36">
+        <div className="flex justify-evenly ">
+          {seriesTitles.map((series) => {
+            return (
+              <button
+                key={series}
+                className="text-5xl  hover:text-slate-900"
+                onClick={() => handleClick(series)}
+              >
+                {series}
+              </button>
+            );
+          })}
+        </div>
+      </div>
+    );
+  };
 
   return (
     <div>
-        <Modal open={open} onClose={ () => setOpen(false)}></Modal>
-        <CreateGallery />
+      <Modal open={open} onClose={() => setOpen(false)}></Modal>
+      <CreateGallery />
     </div>
-  )
+  );
+};
+
+export default ArtGallery;
+
+{
+  /* IMPORTANT: Below is the format for saving art pieces to art.json */
 }
-
-export default ArtGallery
-
-
-{/* IMPORTANT: Below is the format for saving art pieces to art.json */}
 // {
 //     "title": "",
 //     "year": "",
-//     "medium": "", 
+//     "medium": "",
 //     "size": "",
 //     "imgPath": "",
 //     "section": ""
