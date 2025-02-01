e12a714198c204b353c75af0422f5cc4b30c80cf
Aurora Powell <33322696+AuroraPowell@users.noreply.github.com>
2025-02-01 17:40:51 -0500

    Got image gallery working
    Was able to get images to render from file path. Had it in wrong folder; forgot index.html is what is being loaded and didn't have the paths relative to that. haha woops. But yay!

--- client/src/components/functional/ArtGallery.jsx
+++ client/src/components/functional/ArtGallery.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState, useEffect } from "react";
 
-import data from "../art-pieces/art.json";
+import data from "/public/art/art.json";
 import Modal from "../projects/countme.in/functions/Modal";
 
 const ArtGallery = () => {
@@ -16,7 +16,6 @@ const ArtGallery = () => {
   // creates responsively created list from mapping JSON file
   const CreateSidebar = () => {
     let seriesTitles = [];
-    let list = [];
 
     data.map((art) => {
       if (!seriesTitles.includes(art.series)) {
@@ -26,7 +25,7 @@ const ArtGallery = () => {
 
     function handleClick(selection) {
       let value = selection
-      console.log(value)
+      // console.log(value)
       setSeriesFocus(value);
       
     }
@@ -39,7 +38,13 @@ const ArtGallery = () => {
               <button
                 key={list}
                 className="text-5xl  hover:text-slate-900"
-                onClick={() => handleClick(list)}
+                onClick={() => {
+
+                  handleClick(list);
+                  console.log("Series is: " + list)
+                  setArtList(list)                  
+                }
+                }
               >
                 {list}
               </button>
@@ -56,48 +61,53 @@ const ArtGallery = () => {
   const CreateGallery = () => {
     let list = []
 
+    
     // STEPS NEEDED
     // check seriesFocus, load images with same series tag
     // if no seriesFocus, load all images 
     // map artList and create Art objects for each
-    seriesFocus == "" ? data.map((art) => { list.push(art) }) 
-    : (data.map((art) => { 
+
+    seriesFocus == "" ? (data.map((art) => { list.push(art) })) 
+    : createGallery()
+        
+    function createGallery(){
+      data.map( (art) => { 
         if(!list.includes(art)){
           if(art.series == seriesFocus){
             list.push(art)
           }
-        }}))
-
-        // Art Object
-  const Art = (obj) => {
-    console.log(obj)
-    // let title =   artObj.title
-    // let year =    artObj.year
-    // let medium =  artObj.medium
-    // let size =    artObj.size
-    // let url =     artObj.url
-    // let series =  artObj.series
-
-    // console.log(obj)
-    return (
-      // display art, alt text, and hover info
-      <div >
-        <p>{obj.title}</p>
-      </div>
-    )
-  }
+        }
+      }) 
+    }
     
+    const ArtCard = ({art}) => {
+        return(
+          <div className="rounded-xl bg-teal-400 w-fit">
+            <p>{art.title}</p>
+            <img src={art.imgPath} className="h-40"/>
+          </div>
+        )
+    }
+    
+
+    
+
     return (
       <div>
-        {list.map((art) => {return(<Art id={art.title} obj={art} />)})}
+        <ul className="flex flex-row">
+          {list.map((art) => (
+            <ArtCard id={art.title} art={art} />
+          ))}
+        </ul>
       </div>
-    )
-  }
+    )}
+  
 
   return (
     <div>
       <Modal open={modalOpen} onClose={() => setModalOpen(false)}></Modal>
       <CreateSidebar />
+      <CreateGallery />
     </div>
   );
 };
@@ -115,3 +125,25 @@ export default ArtGallery;
 //     "imgPath": "",
 //     "section": ""
 // }
+
+// const Art = (title, year, medium, size, url, series) => {
+
+//   console.log(title)
+  
+//   let artTitle = title
+//   // let year =    artObj.year
+//   // let medium =  artObj.medium
+//   // let size =    artObj.size
+//   // let url =     artObj.url
+//   // let series =  artObj.series
+//   const card = 
+//   (<div>
+//     {artTitle}
+//   </div>)
+//   // console.log(obj)
+//   return (
+//     // display art, alt text, and hover info
+//     <div >
+//       {card}
+//     </div>
+//   )}
